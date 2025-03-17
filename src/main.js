e262cccb4c64fd43290bccb8c54724c250cc23ac
Hirokazu Hirono <hirono.hirokazu@gmail.com>
2025-03-17 22:00:29 +0900

    initial commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import "./css/style.css";
+import { Elm } from "./Main.elm";
+
+if (process.env.NODE_ENV === "development") {
+    const ElmDebugTransform = await import("elm-debug-transformer");
+
+    ElmDebugTransform.register({
+        simple_mode: true,
+    });
+}
+
+const root = document.querySelector("#app");
+const app = Elm.Main.init({ node: root });
